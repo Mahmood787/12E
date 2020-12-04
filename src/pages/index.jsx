65f0eb4d52c73c29f9c8a85b903a5fa87d039745
Mahmood787 <ali_khan87@live.com>
2020-12-04 15:15:29 +0500

    build hook

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,6 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Header/>
+      k
       <div className="lolliList">
         <div>
           <Lollypop fillLollyBottom="blue" fillLollyTop="green" fillLollyMiddle="black"/>
