4a136b1b882d31b9811e5a9f4f6968d8e28a89f0
Mahmood787 <ali_khan87@live.com>
2020-12-05 23:46:40 +0500

    build hook added

--- src/pages/createNew.tsx
+++ src/pages/createNew.tsx
@@ -56,7 +56,7 @@ export default function CreateNew  ()  {
     }
     useEffect(() => {
         async function runHook() {
-             await fetch("https://api.netlify.com/build_hooks/5fc54a1a0d7a9d2ac1c1ba3f", {
+             await fetch("https://api.netlify.com/build_hooks/5fcbd551d9f2fa98f0b34623", {
                 method: "POST",
             });
         }
