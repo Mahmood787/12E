b37bb23993b4ec82c0999d8fc4c4ad12b3af5fc9
Mahmood787 <ali_khan87@live.com>
2020-12-05 23:37:35 +0500

    types added

--- gatsby-node.js
+++ gatsby-node.js
@@ -14,7 +14,7 @@ exports.createPages =async ({actions, graphql})=>{
     data.Lollies.getAllLollies.forEach(({lollyPath})=>{
         actions.createPage({
             path:`lollies/${lollyPath}`,
-            component: path.resolve(`./src/components/Template.jsx`),
+            component: path.resolve(`./src/components/Template.tsx`),
             context:{
                 lollyPath,
             }
